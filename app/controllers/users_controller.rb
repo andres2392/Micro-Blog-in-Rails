681b57cf63eef33d34f50c36715b2441b5bfbfdc
Andres Gonzalez <andres.gonzalez2392@gmail.com>
2020-06-11 13:57:31 -0400

    Add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to the Micro blog #{user.username}, you have succesfully sign up"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
+end
+private
+def user_params
+    params.require(:user).permit(:username, :email, :password)
 end
