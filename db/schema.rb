0337b9be9679761e985042d054a71c6246f1d23b
Andres Gonzalez <andres.gonzalez2392@gmail.com>
2020-06-16 16:46:51 -0400

    Revert "Web done"
    This reverts commit 4fe67c5e423659754557245053e1ec1f8ecf23ef.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_163900) do
+ActiveRecord::Schema.define(version: 2020_06_11_164233) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_06_16_163900) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.boolean "admin", default: false
   end
 
 end
